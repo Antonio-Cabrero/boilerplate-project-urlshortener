cb98ff65ca71e2d3d9838a0243dcb1a858694821
Antonio Cabrero <49397161+Antonio-Cabrero@users.noreply.github.com>
2025-02-03 07:52:52 +0100

    moved validateUrl to its own function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const validUrl = require("valid-url");
 const mongoose = require("mongoose");
+const dns = require("dns");
 
 const urlSchema = new mongoose.Schema({
 	original_url: String,
@@ -13,34 +14,54 @@ const findURL = async (original_url) => {
 	return await URL.findOne({ original_url });
 };
 
-router.post("/shorturl", async (req, res) => {
+function validateURL(url) {
+	const submittedUrl = url;
 	try {
-		const { url: original_url } = req.body;
-		console.log(original_url);
-		if (!validUrl.isUri(original_url)) {
-			return res.json({ error: "invalid url" });
+		console.log("submittedUrl", submittedUrl);
+		if (submittedUrl === null || submittedUrl === "") {
+			return false;
 		} else {
-			// check if url already exists in database
-			let url = await findURL(original_url);
-			if (url) {
-				console.log("found");
-				res.json(url.short_url);
+			const dnsValid = dns.lookup(submittedUrl, (err, addr) => {
+				if (err || !addr) {
+					return false;
+				} else {
+					return true;
+				}
+			});
+			if (!dnsValid && !validUrl.isUri(submittedUrl)) {
+				return false;
 			} else {
-				// shortenURL
-				const urls = await URL.find();
-
-				// write to database
-				url = new URL({
-					short_url: urls.length,
-					original_url,
-				});
-				await url.save();
-				console.log("saved");
-				res.json({ original_url: url.original_url, short_url: url.short_url });
+				return true;
 			}
 		}
-	} catch (err) {
-		return res.json({ error: "invalid url" });
+	} catch (error) {
+		return false;
+	}
+}
+
+router.post("/shorturl", async (req, res) => {
+	const { url: original_url } = req.body;
+	const isValid = validateURL(original_url);
+	console.log("isValid", isValid);
+	if (isValid) {
+		// check if url already exists in database
+		let url = await findURL(original_url);
+		if (url) {
+			console.log("found");
+			res.json(url.short_url);
+		} else {
+			// shortenURL
+			const urls = await URL.find();
+
+			// write to database
+			url = new URL({
+				short_url: urls.length,
+				original_url,
+			});
+			await url.save();
+			console.log("saved");
+			res.json({ original_url: url.original_url, short_url: url.short_url });
+		}
 	}
 });
 
